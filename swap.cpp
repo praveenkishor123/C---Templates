277a019be3a3b56a5c072e84ce8960ccc6cf64f4
Praveen Kishor <praveen.kishor123@gmail.com>
2014-07-22 11:50:12 +0530

    Create swap.cpp

---
+++ swap.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+void fun(int m, int n, float a, float b)
+{
+	cout << "m and n before swap:"<< m << " " << n << "\n";
+	swap(m,n);
+	cout << "m and n after swap:"<< m << " " << n << "\n";
+
+	cout << "a and b before swap:"<< a << " " << b << "\n";
+	swap(a,b);
+	cout << "a and b after swap:"<< a << " " << b << "\n";
+
+}
+
+template<class T>
+
+void swap(T &x, T &y)
+{
+	T temp = x;
+	x = y;
+	y = temp;
+}
+
+
+int main()
+{
+	fun(100, 200, 11.22, 33.44);
+	return 0;
+}
