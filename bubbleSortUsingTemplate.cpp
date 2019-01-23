98ccec4aba2f8074f83126c9300c80c3846b5611
Praveen Kishor <praveen.kishor123@gmail.com>
2019-01-23 15:17:09 -0800

    Update and rename bubblesortusingtemplates.cpp to bubbleSortUsingTemplate.cpp

--- bubbleSortUsingTemplate.cpp
+++ bubbleSortUsingTemplate.cpp
@@ -5,27 +5,21 @@ template<class T>
 void bubble(T a[], int n)
 {
 	for(int i=0; i < n-1; i++)
-		
 		for(int j=n-1; i < j; j--)
-
-			if(a[j] < a[j-1])
-			{
+			if(a[j] < a[j-1]){
 				swap(a[j], a[j-1]); // calls template function
 			}
 }
 
-
-template<class X>
-void swap(X &a, X &b)
-{
+template<class X> 
+void swap(X &a, X &b) {
 	X temp = a;
 	a = b;
 	b = temp;
 }
 
 
-int main()
-{
+int main() {
 	int x[5] = {10, 50, 30, 40, 20};
 	float y[5] = {1.1, 5.5, 3.3, 4.4, 2.2};
 
@@ -41,7 +35,5 @@ int main()
 	for(int j=0; j<5; j++)
 		cout << y[j] << " ";
 	cout << endl;
-
 	return 0;
 }
-
